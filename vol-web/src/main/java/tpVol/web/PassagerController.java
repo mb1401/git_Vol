ff2ba3edce56d10b150c346b363daa69b95ee6f3
Unknown <mathias_moys@hotmail.com>
2018-06-06 10:44:58 +0200

    ajout passager

---
+++ vol-web/src/main/java/tpVol/web/PassagerController.java
@@ -0,0 +1,73 @@
+package tpVol.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import vol.model.Passager;
+import vol.repository.PassagerDao;
+
+@Controller
+@RequestMapping("/passager")
+public class PassagerController {
+
+	@Autowired
+	private PassagerDao passagerDao;
+
+	@GetMapping("/")
+	public String home() {
+		return "forward:list";
+	}
+
+	@GetMapping("/list")
+	public String list(Model model) {
+		List<Passager> liste = passagerDao.findAll();
+
+		model.addAttribute("passagers", liste);
+
+		return "/passager/list";
+	}
+
+	@GetMapping("/add")
+	public ModelAndView add() {
+		return new ModelAndView("/passager/edit", "passager", new Passager());
+	}
+
+	public String edit(@RequestParam Long id, Model model) {
+		Optional<Passager> passager = passagerDao.findById(id);
+
+		if (passager.isPresent()) {
+			model.addAttribute("passager", passager.get());
+		} else {
+			model.addAttribute("passager", new Passager());
+		}
+		return "/passager/edit";
+	}
+
+	@PostMapping("/save")
+	public String save(@ModelAttribute("Passager") Passager passager) {
+		passagerDao.save(passager);
+		return "redirect:list";
+
+	}
+
+	public String delete(@RequestParam Long id) {
+		passagerDao.deleteById(id);
+		return "forward:list";
+	}
+
+	@GetMapping("/cancel")
+	public String cancel() {
+		return "forward:list";
+	}
+
+}
---
+++ vol-web/src/main/webapp/WEB-INF/views/jsp/passager/edit.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>    
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta charset=UTF-8">
+<link rel="stylesheet" href="<c:url value="/css/bootstrap.min.css"/>">
+<script src="<c:url value="/js/jquery.min.js"/>"></script>
+<script src="<c:url value="/js/bootstrap.min.js"/>"></script>
+<title>Edition du Passager</title>
+</head>
+<body>
+	<div class="container">
+		<fieldset>
+   			<legend>Edition du Passager</legend>
+			<form:form modelAttribute="passager" action="save" method="post">
+				<form:hidden class="form-control" path="id"/> 
+				<form:hidden class="form-control" path="version"/> 
+				 <div class="form-group">
+				<form:label path="nom">Nom:</form:label> <form:input type="text" class="form-control" path="nom"/>
+				 </div>
+				 <div class="form-group">
+				<form:label path="prenom">Prénom:</form:label> <form:input type="text" class="form-control" path="prenom"/>
+				</div>
+				<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-ok"></span></button>
+				<a href="<c:url value="/passager/cancel"/>" class="btn btn-warning"><span class="glyphicon glyphicon-remove"></span></a>
+			</form:form>
+		</fieldset>
+	</div>
+</body>
+</html>
+
+</body>
+</html>
